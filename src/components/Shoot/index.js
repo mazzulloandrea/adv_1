001fc83bbcdf83a1f82e617106b2df72da8d8ea2
Andrea Mazzullo <andrea.mazzullo@spindox.it>
2022-07-24 18:53:04 +0200

    typo

--- src/components/Shoot/index.js
+++ src/components/Shoot/index.js
@@ -43,7 +43,7 @@ function Shoot() {
   }, []);
 
   useEffect(() => {
-    console.log('counter -1', counter);
+    // console.log('counter -1', counter);
   }, [counter]);
 
   const generate = () => {
