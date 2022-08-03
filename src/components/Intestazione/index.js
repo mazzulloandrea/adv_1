86d1c007aee083df64e9159d8b02581c1236af97
Andrea Mazzullo <andrea.mazzullo@spindox.it>
2022-08-03 18:41:01 +0200

    add abilita inside header

--- src/components/Intestazione/index.js
+++ src/components/Intestazione/index.js
@@ -1,10 +1,58 @@
+import { html } from 'htm/preact';
 import style from './style.css';
+import Cuore from '../../assets/icons/cuore.svg';
+import { Header, Abilita, Title, Zaino, Cuori, AbilitaContainer } from './styled.js';
 
-const Intestazione = ({ title }) => (
-  <header class={style.header}>
-    <h1 class={style.nomargin}>{title}</h1>
+const Intestazione = ({ abilita, title }) => {
+  const { corpo, mente, spirito, vita, zaino } = abilita;
 
-  </header>
-);
+  function getAbilita() {
+    return html`
+    <${AbilitaContainer}>
+      <div>
+        <div>C</div>
+        <div>${corpo}</div>
+      </div>
+      <div>
+        <div>M</div>
+        <div>${mente}</div>
+      </div>
+      <div>
+        <div>S</div>
+        <div>${spirito}</div>
+      </div>
+      </ />`
+  };
+
+  function getVita() {
+    const cuori = new Array(vita).fill(0);
+    return html`
+    <${Cuori}>
+      ${cuori.map(c => html`
+      <${Cuore} />`)}
+      </ />
+      
+    `;
+  }
+
+  function getZaino() {
+    return html`
+      ${zaino.map(z => html`<div>${z.nome}</div>`)}
+    `
+  }
+
+  return html`
+    <${Header}>
+      <${Abilita}>
+        ${getAbilita()}
+        ${getVita()}
+        </ />
+    
+        <${Title}>${title}</ />
+          <${Zaino}>${getZaino()}</ />
+    
+            </ />
+  `
+};
 
 export default Intestazione;
